c6e9ad14ecd55ff31f7e2999f408ffc147dcc154
Sander Hidding <sander.hidding@greenberry.nl>
2022-04-21 22:01:49 +0200

    🎨 small improvement on validation

--- src/util/debug/_error.js
+++ src/util/debug/_error.js
@@ -8,9 +8,21 @@ import _isArray from '@util/check/_isArray';
     Error
  ---------------------------------------- */
 
+/**
+ * A simple schema type validator for matching
+ * types. Used for type validation in objects, 
+ * will return a closure that can validate a given
+ * object. The closure will resolve a boolean
+ * that will output the resolved checks. 
+ * 
+ * @param { bool } checks - operation
+ * @param { string } message - optional error message
+ * @return { bool } validate
+ */
+
 const error = (checks, message) => {
     const validated = !message || _isArray(checks) ? checks.every(check => check) : checks;
-    if(validated) return console.error(message) && true;
+    return validated && console.error(message);
 };
 
 /** ----------------------------------------
