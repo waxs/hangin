5f3b147c38a9e25cfbec0081016a8eb7ad57ba8d
Sander Hidding <sander.hidding@greenberry.nl>
2024-01-30 12:19:06 +0100

    ⚡️ update blocking

--- src/methods/handle/_plan.js
+++ src/methods/handle/_plan.js
@@ -4,7 +4,21 @@
 
 import _cron from '@util/format/_cron';
 import _remainder from '@util/format/_remainder';
-import timeout from '@util/function/_timeout';
+import _timeout from '@util/function/_timeout';
+import _timestamp from '@util/format/_timestamp';
+
+/** ----------------------------------------
+    Cancel
+ ---------------------------------------- */
+
+const cancel = ({ limit, start, end, times }) => {
+    const remainder = _remainder(end?.unix);
+
+    const _limit = limit <= times;
+    const _remain = (start && end) && remainder < 0;
+
+    return [_limit, _remain].some(value => value === true);
+};
 
 /** ----------------------------------------
     Plan
@@ -23,9 +37,22 @@ function plan() {
     const schedule = this.model(this._schedule);
     
     if(schedule && schedule.active && this.active) {
-        const { events, cron, routine } = schedule;
-        
-        const amount = _remainder(_cron(cron));
+        const { events, cron, routine, limit, times } = schedule;
+
+        const block = cancel(schedule);
+
+        if(block) {
+            this.event.dispatch('completed');
+
+            this.apply({
+                active: false,
+                stop: _timestamp()
+            });
+
+            return;
+        }
+
+        const amount = cron && _remainder(_cron(cron));
 
         const exe = () => {
             const queue = this.queue(schedule, events);
@@ -35,13 +62,14 @@ function plan() {
             });
         };
 
-        timer = timeout(() => exe(), cron ? amount : schedule.unix);
+        timer = _timeout(() => exe(), cron ? amount : schedule.unix);
 
         this.apply({ 
             timeout: timer.fn,
-            times: schedule.times + 1,
+            // remain: limit ? limit - times : null,
+            times: times + 1,
             planned: _cron(cron),
-            next: routine === 'loop' && amount 
+            next: routine === 'loop' && amount
         });
 
         this.planned = schedule;
